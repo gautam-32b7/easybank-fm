677bae12c93d253c91671aa97cc3a84a5d636ecc
Gautam Sarkar <gautam_32b7@hotmail.com>
2025-01-25 07:31:42 +0530

    implement sticky navigation using IntersectionObserver API

--- js/script.js
+++ js/script.js
@@ -1,6 +1,26 @@
 const header = document.querySelector("header");
 const navButton = document.querySelector(".btn-mobile-nav");
+const hero = document.querySelector(".hero");
 
+// Hamburger menu
 navButton.addEventListener("click", function () {
   header.classList.toggle("nav-open");
 });
+
+// Sticky navigation
+function stickyNav(entries) {
+  const [entry] = entries;
+
+  if (!entry.isIntersecting) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+}
+
+const heroObserver = new IntersectionObserver(stickyNav, {
+  root: null,
+  threshold: 0.5,
+});
+
+heroObserver.observe(hero);
